742cd99991beb21d370a022a969a9e94392afb04
Teemu Pääkkönen <paakkonen.teemu@gmail.com>
2026-02-06 11:27:48 +0200

    stuff

--- example/client/index.tsx
+++ example/client/index.tsx
@@ -3,4 +3,15 @@ import createConnection from "../../lib/createConnection";
 
 const connection = createConnection<Api>("ws://localhost:3000");
 
-connection.getSquare(5).then(console.log);
+connection.getSquare(123).then(console.log)
+
+
+connection.waitAndReverse('string').then(console.log)
+
+connection.getValueFromDeepObject({
+  foo: {
+    bar: {
+      value: 'test'
+    }
+  }
+}).then(console.log)
--- example/server/api.ts
+++ example/server/api.ts
@@ -4,6 +4,16 @@ const api = createApi({
   getSquare(num: number) {
     return num ** 2;
   },
+
+  async waitAndReverse(str: string) {
+    await new Promise(resolve => setTimeout(resolve, 2000))
+    return str.split('').reverse().join('')
+  },
+
+  getValueFromDeepObject(obj: { foo: { bar: { value: string } } }) {
+    return { message: `The value is "${obj.foo.bar.value}"` }
+  }
+
 });
 
 type Api = CreateApiDefinitionType<typeof api>;
